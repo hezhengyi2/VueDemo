3f1b031b828e775d7b1cabf6a5f7a06507af54f0
hezhengyi2 <hezhengyiwork@163.com>
2020-07-02 17:51:49 +0800

    修改测试提交

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-this is atest js 
+var mime=require('mime');
+var parent=require('./parent');
+
+console.log(mime.getType('/path/to/file.txt'));                 // => 'html'
+console.log(mime.getExtension('application/octet-stream'));     // => 'bin'
+parent.world();
